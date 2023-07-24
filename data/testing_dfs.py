d388b6bb243ecad6e55d3d8ce1bdc78c09dbc81d
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-07-24 06:18:10 +0100

    added the data scraping app

---
+++ data/testing_dfs.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss. Elizabeth",
+        ],
+        "Age": [22, 35, 58],
+        "Sex": ["male", "male", "female"],
+    }
+)
+
+series1 = pd.Series([12, 34, 56], name="Age")
+
+
+print(df["Age"].describe())
