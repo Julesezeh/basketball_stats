349f337268f27f43c06e9e0de26d83241f52af13
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-09-22 21:09:58 +0100

    About to store different splits in different csv files

--- data/scraper2.py
+++ data/scraper2.py
@@ -4,4 +4,13 @@ url = "https://www.basketball-reference.com/players/t/tatumja01/splits/2023"
 
 df = pd.read_html(url, header=0)
 
-print(df)
+# with open("tatum_data.csv", "a+") as tatum:
+#     for x in df:
+#         tatum.write(x.to_csv)
+# tatum.close()
+
+# def save_data():
+#     pass
+
+for x in df:
+    print(x)
